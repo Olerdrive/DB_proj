443e9001b4939c3f4dbb65769b10a37e5ecfe0c5
Alexey Mokeev <olerdrive@gmail.com>
2016-03-18 14:40:44 +0300

    Clear method finished

--- DBConfig.py
+++ DBConfig.py
@@ -3,3 +3,12 @@ DB_PASSWORD = "db_pass"
 DB_NAME = "projdb"
 DB_CHARSET = "utf8"
 DB_HOST = "localhost"
+
+STATUS_CODE = {
+    'OK': {'status_code': 0},
+    'NOT_FOUND': {'status_code': 1, 'error': 'object not found'},
+    'INVALID_REQUEST': {'status_code': 2, 'error': 'invalid request'},
+    'WRONG_REQUEST': {'status_code': 3, 'error': 'wrong request'},
+    'UNKNOWN_ERROR': {'status_code': 4, 'error': 'unknown error'},
+    'ALREADY_EXISTS': {'status_code': 5, 'error': 'object already exists'}
+}
--- app/general.py
+++ app/general.py
@@ -1,9 +1,18 @@
 from app import app
 from flask import Flask, request
+import db_tools as DBTools
 from flask import jsonify
-
 from app.db_tools import *
+
 
 @app.route('/clear/', methods=['GET'])
 def clear():
-	connection = connect()
+    connection = DBTools.connect()
+    tables = ['Users', 'Forums', 'Threads', 'Posts', 'Followers', 'Subscriptions']
+    DBTools.execute(connection, "SET global foreign_key_checks = 1;")
+    for table in tables:
+        query = "TRUNCATE TABLE %s;" % table
+        DBTools.execute(connection, query)
+    DBTools.execute(connection, "SET global foreign_key_checks = 1;")
+    connection.close()
+    return jsonify({"code": STATUS_CODE['OK'], "response": 'OK'})
